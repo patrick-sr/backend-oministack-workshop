5bd995fe8c2166dfa741f7a6bc21de66694e199f
Patrick Rosa <patrickclanm@gmail.com>
2019-04-11 22:51:51 -0300

    ajustes no server

--- src/server.js
+++ src/server.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(cors());
 
 const server = require('http').Server(app);
-const io = require('socket.io');
+const io = require('socket.io')(server);
 
-io.on('connection', socket => {
+io.on("connection", socket => {
     socket.on('connectRoom', box => {
         socket.join(box);
     })
